127bed729ac61eecb91c161c15b1dfbe7088d200
Warodom Werapun <wwarodom@gmail.com>
2018-02-26 13:34:33 +0700

    update

--- src/components/employee.js
+++ src/components/employee.js
@@ -10,7 +10,7 @@ class Employee extends Component {
     handleValue = (e) => {
         this.setState({[e.target.name]: e.target.value},
             () => {
-                console.log(this.state)
+                // console.log(this.state)
             });
     }
 
